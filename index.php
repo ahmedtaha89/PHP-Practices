68a2689c54c2a83cd4925005a37424b27c958c72
ahmedtaha89 <95527363+ahmedtaha89@users.noreply.github.com>
2022-08-07 17:52:50 +0200

    this is the first practice (1)

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+</head>
+<body>
+
+<?php 
+
+// Type of command 
+# (hash) use in short notes (single line)
+// two forward slashes      (single line)
+/* multi comments use in longer notes (multi lines) */ 
+
+
+
+
+#this is the first practice
+
+//Practice(01)
+header('Content-Type:text/html; charset = UTF-8');
+echo '<title> My First PHP Page  </title>';
+echo '<div> We Love </div>';
+echo '<div> Elzero Channel   </div>';
+?>
+
+
+<?php 
+// Practice(02)
+// echo 'Prevent Me From Running Please';
+#echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please';*/
+
+
+
+// Practice(03)
+/* My Application
+  Version 1.0
+ Created By Elzero */
+
+
+
+// Practice(04)
+ // ## First Comment  
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment  T
+
+
+?>
+
+</body>
+</html>
